7e77bc56029e75eee469ce35c95843deeb3ba8f6
Jon Granados <grxnxdos@gmail.com>
2023-01-03 11:18:28 -0800

    Implemented custom error-handling middleware.

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize'); 
 
 /* CONFIGURATIONS */
 const { environment } = require('./config');
@@ -18,12 +19,44 @@ app.use(express.json());
 
 if (!isProduction) {
   app.use(cors());
-}
+};
 
 app.use(
   helmet.crossOriginResourcePolicy({ 
     policy: "cross-origin" 
   })
 );
+
+/* ERROR HANDLING */
+// catch unhandled requests and forward to error handler
+app.use((_req, _res, next) => { 
+  const err = new Error(`The requested resource couldn't be found.`);
+  err.title = 'Resource Not Found'; 
+  err.errors = [`The requested resource couldn't be found.`];
+  err.status = 404; 
+  
+  next(err); 
+});
+
+// process sequelize errors
+app.use((err, _req, _res, next) => { 
+  if (err instanceof ValidationError) { 
+    err.errors = err.errors.map((e) => e.message); 
+    err.title = 'Sequelize validation error'; 
+  }
+
+  next (err); 
+}); 
+
+// custom error handler
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500); 
+  res.json({ 
+    title: err.title || 'Server error', 
+    message: err.message, 
+    errors: err.errors, 
+    stack: isProduction ? null : err.stack,
+  }); 
+}); 
 
 module.exports = app; 
