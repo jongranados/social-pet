e6b799dcd09bb307950378aa50d7f86e0aa28a86
Jon Granados <grxnxdos@gmail.com>
2023-01-02 23:06:40 -0800

    Setup sequelize.

---
+++ backend/config/database.js
@@ -0,0 +1,27 @@
+const config = require('./index'); 
+
+const db = config.db; 
+const { username, password, database, hose } = db; 
+
+module.exports = { 
+  development: { 
+    username, 
+    password, 
+    database, 
+    hose, 
+    dialect: 'postgres', 
+    seederStorage: 'sequelize'
+  }, 
+
+  production: { 
+    use_env_variables: 'DATABASE_URL', 
+    dialect: 'postgres', 
+    seederStorage: 'sequelize', 
+    dialectOptions: { 
+      ssl: { 
+        require: true, 
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
