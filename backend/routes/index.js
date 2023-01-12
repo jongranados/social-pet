d57c1481162444b797b0c036adea2965cd681b6c
Jon Granados <grxnxdos@gmail.com>
2023-01-11 20:18:59 -0800

    Added router for API routes.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,17 @@
 const express = require('express'); 
 const router = express.Router(); 
+const apiRouter = require('./api'); 
 
-// test route for checking functionality of csurf middleware  
+/* TEST ROUTES */
+// to test functionality of csurf middleware  
 router.get('/hello/world', function(req, res) {
     const xsrfToken = req.csrfToken()
     res.cookie('XSRF-TOKEN', xsrfToken);
     res.send('Hello World!');
 });
+
+/* API ROUTER MOUNTING */
+router.use('/api', apiRouter);
+
 
 module.exports = router; 
