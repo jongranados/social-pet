5943daa64f020439b440caf560560bff6ab70601
Jon Granados <grxnxdos@gmail.com>
2023-01-17 15:41:35 -0800

    Implemented authentication middleware utility functions.

---
+++ backend/middleware/auth.js
@@ -0,0 +1,67 @@
+const jwt = require('jsonwebtoken'); 
+const { jwtConfig } = require('../config'); 
+const { secret, expiresIn } = jwtConfig; 
+const { User } = require('../db/models'); 
+
+// creates a JWT cookie and sets it on the res obj. called upon successful login or signup. 
+const setTokenCookie = (res, user) => { 
+    const token = jwt.sign(
+        { data: user.toSafeObject()}, 
+        secret, 
+        { expiresIn: parseInt(expiresIn)} 
+    ); 
+
+    const isProduction = process.env.NODE_ENV === 'production'; 
+
+    res.cookie('token', token, { 
+        maxAge: expiresIn * 1000, 
+        httpOnly: true, 
+        secure: isProduction, 
+        sameSite: isProduction && 'Lax'
+    });
+
+    return token;
+}; 
+
+// restores session user to req.user based upon the payload of the session JWT cookie
+const restoreSessionUser = (req, res, next) => { 
+    const { token } = req.cookies; 
+
+    return jwt.verify(
+        token, 
+        secret, 
+        null, 
+        async (err, jwtPayload) => { 
+            if (err) return next(); 
+
+            try { 
+                const { id } = jwtPayload.data; 
+                req.user = await User.scope('currentUser').findByPk(id); 
+            } catch (e) { 
+                res.clearCookie('token'); 
+                return next(); 
+            }
+
+            if (!req.user) res.clearCookie('token'); 
+
+            return next(); 
+        }
+    ); 
+}; 
+
+// two part middleware defined as an array. the first middleware attempts to restore the session user by leveraging restoreSessionUser. the second verifies that a session user was restored.
+const requireAuthentication = [ 
+    restoreSessionUser, 
+    function (req, _res, next) { 
+        if (req.user) return next(); 
+
+        const err = new Error('Unauthorized'); 
+        err.title = 'Unauthroized'; 
+        err.errors = ['Unauthorized']; 
+        err.status = 401; 
+        return next(err); 
+    }
+]; 
+
+
+module.exports = { setTokenCookie, restoreSessionUser, requireAuthentication };
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,4 +6,40 @@ router.post('/test', function(req, res) {
     res.json({ requestBody: req.body }); 
 });
 
+const asyncHandler = require('express-async-handler'); 
+const { setTokenCookie, restoreSessionUser, requireAuthentication } = require('../../middleware/auth.js'); 
+const { User } = require('../../db/models'); 
+
+// GET /api/set-token-cookie
+router.get(
+    '/set-token-cookie', 
+    asyncHandler(async (_req, res) => { 
+        const user = await User.findOne({ 
+            where: { 
+                username: 'username5'
+            }
+        }); 
+
+        setTokenCookie(res, user); 
+        return res.json({ user }); 
+    })
+); 
+
+// GET /api/restore-user
+router.get(
+    '/restore-session-user', 
+    restoreSessionUser, 
+    (req, res) => { 
+        return res.json(req.user);    
+    }
+); 
+
+// GET /api/require-auth
+router.get(
+    '/require-authentication', 
+    requireAuthentication, 
+    (req, res) => { 
+        return res.json(req.user);    
+    }
+); 
 module.exports = router; 
