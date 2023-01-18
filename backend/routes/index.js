83a746e8e3dbe115b7a5f50789f025f2aa88d0d3
Jon Granados <grxnxdos@gmail.com>
2023-01-17 20:37:07 -0800

    Implemented user login route.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express'); 
 const router = express.Router(); 
 const testRouter = require('./test'); 
+const authRouter = require('./auth'); 
 
 /* TEST ROUTER MOUNTING */
 router.use('/test', testRouter);
+
+/* AUTHENTICATION ROUTER MOUNTING */
+router.use('/auth', authRouter);
 
 module.exports = router; 
