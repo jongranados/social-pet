2aa40b22c751cdef8472c2cca99ada0b6b8d9e5b
Jon Granados <grxnxdos@gmail.com>
2023-01-18 23:03:26 -0800

    Deleted sample fetch request.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,16 +10,4 @@ router.post('/login', asyncHandler(login));
 router.post('/signup', asyncHandler(signup))
 
 
-
-// fetch('/auth/signup', {
-//   method: 'POST',
-//   headers: {
-//     "Content-Type": "application/json",
-//     "XSRF-TOKEN": `ngA4vkt7-AcmSOUgcKHqG_PSQmWviNlffKfI`
-//   },
-//   body: JSON.stringify({ firstName: 'fname31', lastName: 'lname31', username: 'username31', email: 'email31@gmail.com', password: 'password31', picturePath: 'p31', gotchaDate: '2022-12-12', breed: 'breed31', location: 'location31', bio: 'This is meant to serve as the bio for user31.' })
-// }).then(res => res.json()).then(data => console.log(data));
-
-
-
 module.exports = router; 
