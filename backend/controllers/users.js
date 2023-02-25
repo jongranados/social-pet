2dfab522ea2eb2c6e6511f3a7e86d88e96ff0b1b
Jon Granados <grxnxdos@gmail.com>
2023-02-24 22:42:15 -0800

    Implemented get-users-follows route.

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,4 +1,4 @@
-const { User } = require('../db/models'); 
+const { User, Follow } = require('../db/models'); 
 
 /* READ CONTROLLERS */
 const getUser = async (req, res, next) => { 
@@ -18,4 +18,33 @@ const getUser = async (req, res, next) => {
     });
 }; 
 
-module.exports = { getUser }; 
+const getUserFollows = async (req, res, next) => { 
+    const { id } = req.params; 
+    const user = await User.getUserById(id); 
+
+    if (!user) { 
+        const err = new Error('User not found.'); 
+        err.status = 404; 
+        err.title = 'User not found.'; 
+        err.errors = ['The queried ']
+    };
+
+    let followsIds = await Follow.findAll({ 
+        where: { 
+            followerId: id
+        }, 
+        attributes: ['followeeId'], 
+        raw: true
+    }); 
+
+    followsIds = followsIds.map(followee => followee.followeeId)
+    
+    // We call Promise.all on the array returned by map to transform it to a single Promise we can then await
+    const follows = await Promise.all(followsIds.map(async followsId => await User.getUserById(followsId)));
+    
+    return res.json({
+        follows
+    }); 
+}; 
+
+module.exports = { getUser, getUserFollows }; 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router(); 
 const asyncHandler = require('express-async-handler'); 
 const { requireAuthentication } = require('../middleware/auth'); 
-const { getUser } = require('../controllers/users'); 
+const { getUser, getUserFollows } = require('../controllers/users'); 
 
 /* READ ROUTES */
 
 // GET /users/:id
 router.get('/:id', requireAuthentication, asyncHandler(getUser)); 
+
+// GET /users/:id/follows
+router.get('/:id/follows', requireAuthentication, asyncHandler(getUserFollows));
 
 module.exports = router;
