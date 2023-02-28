8d23eccf167e83ec05bf7957101ed41047e6ac19
Jon Granados <grxnxdos@gmail.com>
2023-02-27 16:27:51 -0800

    Implemented backend route to update user's following.

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,4 +1,6 @@
 const { User, Follow } = require('../db/models'); 
+const { Op } = require('sequelize'); 
+const e = require('express');
 
 /* READ CONTROLLERS */
 const getUser = async (req, res, next) => { 
@@ -26,7 +28,8 @@ const getUserFollows = async (req, res, next) => {
         const err = new Error('User not found.'); 
         err.status = 404; 
         err.title = 'User not found.'; 
-        err.errors = ['The queried ']
+        err.errors = ['The queried user was not found.']
+        return next(err); 
     };
 
     let followsIds = await Follow.findAll({ 
@@ -47,4 +50,60 @@ const getUserFollows = async (req, res, next) => {
     }); 
 }; 
 
-module.exports = { getUser, getUserFollows }; 
+/* UPDATE CONTROLLERS */
+const updateUserFollowing = async (req, res, next) => { 
+    const { id, followeeId } = req.params; 
+    const user = await User.getUserById(id); 
+    const followee = await User.getUserById(followeeId); 
+
+    if (!user) { 
+        const err = new Error('User not found.'); 
+        err.status = 404; 
+        err.title = 'User not found.'; 
+        err.errors = ['The queried user was not found.']
+        return next(err); 
+
+    }; 
+
+    if (!followee) { 
+        const err = new Error('Followee not found.'); 
+        err.status = 404; 
+        err.title = 'Followee not found.'; 
+        err.errors = ['The queried user was not found.']
+        return next(err); 
+
+    }; 
+    
+    // query db for relationship status. 
+    const relationship = await Follow.findOne({ 
+        where: { 
+            [Op.and]: [{ followerId: id }, { followeeId }] 
+        } 
+    }); 
+    
+    // if relationship exists, unfollow. else, follow.
+    if (relationship) await relationship.destroy(); 
+    else await Follow.create({ followerId: id, followeeId }); 
+
+    // retrive the updated list of ids for accounts that the user follows. 
+    let followingIds = await Follow.findAll({ 
+        where: { 
+            followerId: id
+        }, 
+        attributes: ['followeeId'], 
+    }); 
+
+    // retrieve the profiles of corresponding ids. 
+    const following = await Promise.all(followingIds.map(async followsId => { 
+        const id = followsId.followeeId; 
+        const user = User.getUserById(id); 
+        return user; 
+    }));
+    
+    // return profiles
+    return res.json({
+        following
+    }); 
+}; 
+
+module.exports = { getUser, getUserFollows, updateUserFollowing }; 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router(); 
 const asyncHandler = require('express-async-handler'); 
 const { requireAuthentication } = require('../middleware/auth'); 
-const { getUser, getUserFollows } = require('../controllers/users'); 
+const { getUser, getUserFollows, updateUserFollowing } = require('../controllers/users'); 
 
 /* READ ROUTES */
 
@@ -11,5 +11,11 @@ router.get('/:id', requireAuthentication, asyncHandler(getUser));
 
 // GET /users/:id/follows
 router.get('/:id/follows', requireAuthentication, asyncHandler(getUserFollows));
+
+
+/* UPDATE ROUTES */
+
+// PATCH /:id/:followeeId
+router.patch('/:id/:followeeId', requireAuthentication, asyncHandler(updateUserFollowing));
 
 module.exports = router;
