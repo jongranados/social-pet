4769f013eaf4762f95d0c2f16d5212c457272bfe
Jon Granados <grxnxdos@gmail.com>
2023-03-16 23:08:12 -0700

    Added routing using React Router.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import HomePage from 'scenes/homePage'; 
+import LoginPage from 'scenes/loginPage'; 
+import ProfilePage from 'scenes/profilePage'; 
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <div className="app">
+      <BrowserRouter> 
+        <Routes> 
+          <Route path='/' element={<LoginPage />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/profile/:userId' element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
