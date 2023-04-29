3160aff4fa591b110ca0fbf872c9b9c49c8e4215
Jon Granados <grxnxdos@gmail.com>
2023-04-28 23:46:48 -0700

    create file structure for SignupPage component

---
+++ frontend/src/scenes/signupPage/Form.jsx
@@ -0,0 +1,17 @@
+import { Formik } from 'formik';
+
+const Form = () => {
+
+    return (
+        <Formik>
+            {() => (
+                <form>
+                    this is a form
+                </form>
+            )}
+        </Formik>
+    )
+
+};
+
+export default Form; 
---
+++ frontend/src/scenes/signupPage/index.jsx
@@ -0,0 +1,12 @@
+import Form from './Form';
+
+const SignupPage = () => {
+    return (
+        <>
+            <p>hello from the signup page</p>
+            <Form />
+        </>
+    )
+};
+
+export default SignupPage; 
