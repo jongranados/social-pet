dcb8293a201b57fd82a3fba300b413ae232c8a78
Jon Granados <grxnxdos@gmail.com>
2023-04-30 17:30:48 -0700

    style loginPage form attributes

--- frontend/src/scenes/loginPage/Form.jsx
+++ frontend/src/scenes/loginPage/Form.jsx
@@ -1,19 +1,55 @@
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, TextField, useMediaQuery, Typography, useTheme } from '@mui/material';
 import { Formik } from 'formik'; 
 
 const Form = () => { 
+    const theme = useTheme(); 
+    const { palette } = theme; 
+    const matchesMobileDevice = useMediaQuery('(max-width:600px)'); 
 
     return(
         <Formik>
-            {() => (
+            {({
+                handleBlur, 
+                handleChange, 
+            }) => (
                 <form>
-                    <Box>
-                    <TextField/>
-                    <TextField/>
+                    <Box
+                        display='grid'
+                        gap='30px'
+                        gridTemplateColumns='repeat(2, minmax(0, 1fr))'
+                        sx={{
+                            '& > div': { gridColumn: matchesMobileDevice ? 'span 2' : undefined }
+                        }}
+                    >
+                    <TextField
+                        label='Username or email address'
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        name='credential'
+                        sx={{ gridColumn: 'span 1' }}
+                    />
+                    <TextField
+                        label='Password'
+                        type='password'
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        name='password'
+                        sx={{ gridColumn: 'span 1' }}
+                    />
                     </Box>
 
                     {/* BUTTONS */}
-                    <Button>
+                    <Button
+                        fullWidth
+                        type='submit'
+                        sx={{
+                            margin: '2rem 0', 
+                            padding: '1rem', 
+                            backgroundColor: palette.primary.main,
+                            color: palette.background.alt, 
+                            '&:hover': { color: palette.primary.main }
+                        }}
+                    >
                         LOGIN
                     </Button>
 
