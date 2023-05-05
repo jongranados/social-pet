db8ddffaee6bc9cc24c5248002b1e0161044ec2c
Jon Granados <grxnxdos@gmail.com>
2023-05-04 23:19:43 -0700

    implement SignupPage component

--- frontend/src/scenes/signupPage/index.jsx
+++ frontend/src/scenes/signupPage/index.jsx
@@ -1,11 +1,32 @@
-import Form from './Form';
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material';
+import Form from './SignupForm';
+import FormBanner from 'components/FormBanner';
 
 const SignupPage = () => {
+    const theme = useTheme();
+    const matchesMobileDevice = useMediaQuery('(max-width: 1000px)');
+
     return (
-        <>
-            <p>hello from the signup page</p>
-            <Form />
-        </>
+        <Box>
+            <FormBanner />
+
+            <Box
+                width={matchesMobileDevice ? '93%' : '50%'}
+                padding='2rem'
+                margin='2rem auto'
+                borderRadius='1.5rem'
+                backgroundColor={theme.palette.background.alt}
+            >
+                <Typography
+                    fontWeight='500'
+                    variant='h4'
+                    sx={{ marginBottom: '1.5rem' }}
+                >
+                    Pets: unite! Humans: out of sight!
+                </Typography>
+                <Form />
+            </Box>
+        </Box>
     )
 };
 
