765e4709e62a1480a9e80429368c46ec78dda78d
Jon Granados <grxnxdos@gmail.com>
2023-05-07 23:49:24 -0700

    update signupPage file structure

--- frontend/src/scenes/signupPage/SignupForm.jsx
+++ frontend/src/scenes/signupPage/SignupForm.jsx
@@ -1,6 +1,6 @@
 import { Formik } from 'formik';
 
-const Form = () => {
+const SignupForm = () => {
 
     return (
         <Formik>
@@ -14,4 +14,4 @@ const Form = () => {
 
 };
 
-export default Form; 
+export default SignupForm; 
