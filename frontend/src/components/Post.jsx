b67d7c24b02a4b076be0cdcc5efa68d9e8913c12
Jon Granados <grxnxdos@gmail.com>
2023-09-13 19:56:50 -0700

    feat: implement Post component core structure

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,9 +1,81 @@
-const PostCard = () => { 
+import { Card, CardMedia, Typography, Box, Divider, IconButton, TextField, FormControl, Button  } from "@mui/material";
+import placeholder from './placeholder.png'
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+
+const Post = () => { 
+
     return (
-        <>
-            This is a post.
-        </>
+        <Card variant="outlined"
+        sx={{
+            display: 'flex',
+            flexDirection: {
+            xs: 'column', // mobile
+            sm: 'row', // tablet and up
+            },
+        }}
+        >
+            <CardMedia
+                component="img"
+                src={placeholder}
+            />
+            <Box>
+                <Box> 
+                    <CardMedia
+                        component="img"
+                        src={placeholder}
+                    />
+
+                    <Typography>
+                        username
+                    </Typography>
+                </Box>
+
+                <Divider />
+
+                <Box>
+                    <Box>
+                        <Typography>
+                            This is a description of a picture. This is a description of a picture. This is a description of a picture. This is a description of a picture. This is a description of a picture. This is a description of a picture. This is a description of a picture. This is a description of a picture. This is a description of a picture. This is a description of a picture. This is a description of a picture. 
+                        </Typography>
+
+                        <Typography>
+                            MM/DD/YY
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Typography>
+                            This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. 
+                        </Typography>
+                        <Typography>
+                            This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. 
+                        </Typography>
+                        <Typography>
+                            This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. 
+                        </Typography>
+                        <Typography>
+                            This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. This is a comment. 
+                        </Typography>
+                    </Box>
+                </Box>
+
+                <Divider />
+                <Box> 
+                    <IconButton>
+                        <FavoriteBorder />
+                    </IconButton>
+
+                    <Typography>
+                        ### likes
+                    </Typography>
+                </Box>
+
+                <FormControl>
+                    <TextField />
+                    <Button>POST</Button>
+                </FormControl>
+            </Box>
+        </Card>
     )
 }
 
-export default PostCard;
+export default Post;
