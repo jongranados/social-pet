2aa7e414c570d8962328e5d7ec96dafcce034024
Jon Granados <grxnxdos@gmail.com>
2023-09-22 15:59:40 -0700

    fix: set hasTrigger in Posts seeder

--- backend/db/seeders/20230308101415-demo-posts.js
+++ backend/db/seeders/20230308101415-demo-posts.js
@@ -6,6 +6,7 @@ let options = {};
 if (process.env.NODE_ENV === "production") {
 	options.schema = process.env.SCHEMA;
 	options.tableName = "Posts";
+  options.hasTrigger = true;
 }
 
 module.exports = {
