f41ead53d216bfa3a64525e8a4653fecaf8485f3
Jon Granados <grxnxdos@gmail.com>
2023-10-03 12:14:15 -0700

    feat: logically limit user navigation based on session state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; 
 import HomePage from 'scenes/homePage'; 
 import LoginPage from 'scenes/loginPage'; 
 import SignupPage from 'scenes/signupPage'; 
@@ -18,26 +18,58 @@ function App() {
   // check to see if session user already exists
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
+  const user = useSelector((state) => state.session.user);
 
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
-  return isLoaded && (
-    <div className="app">
-      <BrowserRouter> 
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Routes> 
-            <Route path='/' element={<HomePage />} />
-            <Route path='/signup' element={<SignupPage />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/profile/:userId' element={<ProfilePage />} />
-          </Routes>
-        </ThemeProvider>
-      </BrowserRouter>
-    </div>
+  return (
+		isLoaded && (
+			<div className="app">
+				<BrowserRouter>
+					<ThemeProvider theme={theme}>
+						<CssBaseline />
+						<Routes>
+							<Route
+								path="/"
+								element={
+									user ? (
+										<HomePage />
+									) : (
+										<Navigate to="/login" />
+									)
+								}
+							/>
+							<Route
+								path="/signup"
+								element={
+									user ? <Navigate to="/" /> : <SignupPage />
+								}
+							/>
+							<Route
+								path="/login"
+								element={
+									user ? <Navigate to="/" /> : <LoginPage />
+								}
+							/>
+							<Route
+								path="/profile/:userId"
+								element={
+									user ? (
+										<ProfilePage />
+									) : (
+										<Navigate to="/login" />
+									)
+								}
+							/>
+						</Routes>
+					</ThemeProvider>
+				</BrowserRouter>
+			</div>
+		)
   );
+
 }
 
 export default App;
