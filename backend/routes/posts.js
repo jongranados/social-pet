5b0f6c84fd070fb63f69c1dc34e205b9327e103d
Jon Granados <grxnxdos@gmail.com>
2023-10-04 17:29:07 -0700

    chore: update get-posts controllers, route definitions, and API calls to better conform to RESTful standards

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,14 +2,7 @@ const express = require('express');
 const router = express.Router(); 
 const asyncHandler = require('express-async-handler'); 
 const { requireAuthentication } = require('../middleware/auth'); 
-const { getFeedPosts, getUserPosts, createPost } = require('../controllers/posts'); 
-
-/* READ ROUTES */
-
-// GET /posts/
-router.get('/', requireAuthentication, asyncHandler(getFeedPosts)); 
-router.get('/:userId/posts', requireAuthentication, asyncHandler(getUserPosts)); 
-
+const { createPost } = require('../controllers/posts'); 
 
 /* UPDATE ROUTES */
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const asyncHandler = require('express-async-handler'); 
 const { requireAuthentication } = require('../middleware/auth'); 
 const { getUser, getUserFollowing, updateUserFollowing } = require('../controllers/users'); 
+const { getFeedPosts, getUserPosts } = require('../controllers/posts');
 
 /* READ ROUTES */
 
@@ -11,6 +12,12 @@ router.get('/:id', requireAuthentication, asyncHandler(getUser));
 
 // GET /users/:id/following
 router.get('/:id/following', requireAuthentication, asyncHandler(getUserFollowing));
+
+// GET /users/:id/feed
+router.get('/:id/feed', requireAuthentication, asyncHandler(getFeedPosts)); 
+
+// GET /users/:id/posts
+router.get('/:id/posts', requireAuthentication, asyncHandler(getUserPosts)); 
 
 
 /* UPDATE ROUTES */
