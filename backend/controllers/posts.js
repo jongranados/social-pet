caf04885b96ee55f9b95fe9d1b9942af3ba9fe0e
Jon Granados <grxnxdos@gmail.com>
2023-10-09 13:21:47 -0700

    feat: implement controller for retrieving a single user's posts

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -62,8 +62,42 @@ const getFeedPosts = async (req, res, next) => {
     });
 }; 
 
-//TODO: implement controller dedicated to exclusively querying for user's posts 
-const getUserPosts = async (req, res, next) => {}; 
+const getUserPosts = async (req, res, next) => {
+    const { id } = req.params;
+
+	let posts = await Post.findAll({
+		where: {
+			userId: id,
+		},
+		include: [
+			{
+				model: Comment,
+				include: {
+					model: User,
+					attributes: ["firstName", "lastName", "picturePath"],
+				},
+			},
+			{
+				model: User.scope("userProfile"),
+				attributes: ["username", "picturePath"],
+			},
+		],
+		order: [["id", "DESC"]],
+	});
+
+	if (!posts) {
+		const err = new Error("Posts not found for specified user.");
+		err.status = 404;
+		err.title = "Posts not found for specified user.";
+		err.errors = ["Posts not found for specified user."];
+		return next(err);
+	}
+
+	return res.json({
+		posts,
+	});
+
+}; 
 
 /* UPDATE CONTROLLERS */
 const createPost = async (req, res, next) => { 
