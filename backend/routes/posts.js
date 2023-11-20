663a63533a357b1dfdfa7ff71441d9e1db0b916c
Jon Granados <grxnxdos@gmail.com>
2023-11-20 15:52:04 -0800

    feat: create POST /posts/:postId/comments route to handle new comment submissions

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router(); 
 const asyncHandler = require('express-async-handler'); 
 const { requireAuthentication } = require('../middleware/auth'); 
-const { createPost } = require('../controllers/posts'); 
+const { createPost, createComment } = require('../controllers/posts'); 
 
 /* UPDATE ROUTES */
 
 // POST /posts/
 router.post('/', requireAuthentication, asyncHandler(createPost)); 
+
+// POST /posts/postId/comments
+router.post('/:postId/comments', requireAuthentication, asyncHandler(createComment));
 
 module.exports = router;
