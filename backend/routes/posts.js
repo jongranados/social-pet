ddf51458ed1562f4d3bb120c127cf93049645582
Jon Granados <grxnxdos@gmail.com>
2023-12-04 15:07:30 -0800

    feat: create backend routes to handle liking and unliking requests

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router(); 
 const asyncHandler = require('express-async-handler'); 
 const { requireAuthentication } = require('../middleware/auth'); 
-const { createPost, createComment } = require('../controllers/posts'); 
+const { createPost, createComment, createLike, deleteLike } = require('../controllers/posts'); 
 
 /* UPDATE ROUTES */
 
@@ -11,5 +11,11 @@ router.post('/', requireAuthentication, asyncHandler(createPost));
 
 // POST /posts/postId/comments
 router.post('/:postId/comments', requireAuthentication, asyncHandler(createComment));
+
+// POST /posts/postId/likes
+router.post('/:postId/likes', requireAuthentication, asyncHandler(createLike)); 
+
+// DELETE /posts/postId/likes/likeId
+router.delete('/:postId/likes/:likeId', requireAuthentication, asyncHandler(deleteLike)); 
 
 module.exports = router;
