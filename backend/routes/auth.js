4d8c83d320b045c8a5bb98733aa1ce6006914288
Jon Granados <grxnxdos@gmail.com>
2024-02-16 23:11:30 -0800

    feat: include multer as middleware in signup route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const asyncHandler = require('express-async-handler'); 
 const { login, logout, signup, session } = require('../controllers/auth'); 
 const { requireAuthentication } = require('../middleware/auth.js')
+const uploadFileViaMulter = require("../middleware/multer.js");
 
 // POST /auth/login
 router.post('/login', asyncHandler(login)); 
@@ -11,7 +12,7 @@ router.post('/login', asyncHandler(login));
 router.delete('/logout', asyncHandler(logout)); 
 
 // POST /auth/signup
-router.post('/signup', asyncHandler(signup));
+router.post('/signup', uploadFileViaMulter.single('picture'), asyncHandler(signup));
 
 // GET /auth/
 router.get('/session', requireAuthentication, asyncHandler(session))
